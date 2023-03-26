9a0cb506a941478a5938d7ae8e167db9b7c9a2de
Jose Armando <josearmando@MacBook-Air.local>
2023-03-26 04:58:20 -0700

    Implemented project views structure with pug as well as express for the backend and prismic

---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+    return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        req
+    })
+}
+
+const handleLinkResolver = doc => {
+    return '/'
+}
+
+app.use((req, res, next) => {
+    res.locals.ctx = {
+        endpoint: process.env.PRISMIC_ENDPOINT,
+        linkResolver: handleLinkResolver
+    }
+
+    res.locals.PrismicDOM = PrismicDOM
+
+    next()
+})
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', async (req, res) => {
+    initApi(req).then(api => {
+        api.query(
+            Prismic.Predicates.any('document.type', ['homepage', 'meta'])
+        ).then(response => {
+            const { results } = response
+            const [ homepage, meta ] = results
+
+            console.log(homepage, meta)
+
+            res.render('index', { homepage, meta })
+        })
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
